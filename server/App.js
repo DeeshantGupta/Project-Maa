abd852f286477e5437a41e6a1f2c7bebd2693c37
Anurag19-Pandey <89677430+Anurag19-Pandey@users.noreply.github.com>
2023-04-23 15:23:15 +0530

    commit by Anurag Pandey "recommend food"

--- server/App.js
+++ server/App.js
@@ -6,6 +6,8 @@ const cookieParser = require("cookie-parser");
 
 const http = require("http") ;
 
+const {Server} = require("socket.io") 
+
 const cors = require("cors");
 
 const app = express();
@@ -34,6 +36,19 @@ app.use(cookieParser());
 app.use(CONSTANT.ROUTES.AUTH, authRoute);
 app.use(CONSTANT.ROUTES.USER, userRoute);
 app.use("/scan", ctgRoute);
+
+const io = new Server(server , {
+    cors :{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+});
+
+io.on("connection",(socket)=>{
+    socket.on("send_message",(data)=>{
+       socket.broadcast.emit("receive_message",data) ;
+    })
+})
 
 server.listen(port, () => {
     console.log(`Server is listening on ${port}`);
